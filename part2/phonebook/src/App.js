402c0d823a9de53303145ba8c72972b4bbefaa2f
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-02-15 15:51:52 +0200

    Continue part2/phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ state, onChange }) => {
   return (
@@ -48,15 +49,17 @@ const Numbers = ({ allPersons, nameFilter }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [nameFilter, setNameFilter] = useState("");
+
+  const fetchPeople = () =>
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data));
+
+  useEffect(() => fetchPeople(), []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
