f0c2abc3651b71b1ab139bcb15ddf6c96d767365
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-02-21 20:58:14 +0200

    4.4 & 4.5

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,6 +1,30 @@
 const listHelper = require('../utils/list_helper');
+const blogs = require('./blog_list');
 
 test('dummy returns one', () => {
-  const blogs = [];
   expect(listHelper.dummy(blogs)).toBe(1);
+});
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    expect(listHelper.totalLikes([])).toBe(0);
+  });
+  test("of list with one item equals its' likes", () => {
+    expect(listHelper.totalLikes([blogs[0]])).toBe(7);
+  });
+  test('of a bigger list equals their combined likes', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36);
+  });
+});
+
+describe('favorite blog', () => {
+  test('of empty list is undefined', () => {
+    expect(listHelper.favoriteBlog([])).toEqual(undefined);
+  });
+  test('of list with one item is the item', () => {
+    expect(listHelper.favoriteBlog([blogs[0]])).toEqual(blogs[0]);
+  });
+  test('of a bigger list is one with most likes', () => {
+    expect(listHelper.favoriteBlog(blogs)).toEqual(blogs[2]);
+  });
 });
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,26 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, obj) => acc + obj.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  let mostLikes = -1;
+  let blogWithMostLikes = undefined;
+  for (const blog of blogs) {
+    if (blog.likes > mostLikes) {
+      mostLikes = blog.likes;
+      blogWithMostLikes = blog;
+    }
+  }
+  return blogWithMostLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
