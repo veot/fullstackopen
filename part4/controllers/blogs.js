fe5b30ae469d8feb4e43b052d364caebc773817f
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-02-24 20:51:14 +0200

    4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,19 +1,38 @@
+const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+
+const getTokenFrom = (req) => {
+  const auth = req.get('authorization');
+  if (auth && auth.toLowerCase().startsWith('bearer '))
+    return auth.substring(7);
+  return null;
+};
 
 router.get('/', async (req, res) =>
   res.json(await Blog.find({}).populate('user', { username: 1, name: 1 })),
 );
 
 router.post('/', async (req, res) => {
-  const user = await User.findOne({});
-  console.log(user);
-  req.body.user = user._id;
-  const blog = new Blog(req.body);
+  const token = getTokenFrom(req);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id)
+    return res.status(401).json({ error: 'Token missing or invalid' });
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title: req.body.title,
+    author: req.body.author,
+    url: req.body.url,
+    likes: req.body.likes === undefined ? 0 : req.body.likes,
+    user: user._id,
+  });
+
   const result = await blog.save();
   user.blogs = user.blogs.concat(result);
   await user.save();
+
   res.status(201).json(result);
 });
 
