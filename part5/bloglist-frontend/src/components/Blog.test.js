6ba0431d96b599f4529749a281e8c97454969055
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-03-16 19:14:25 +0200

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
-test('displays correctly', () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'Test Title 1',
     author: 'Test Author 1',
@@ -12,12 +12,24 @@ test('displays correctly', () => {
     likes: 10,
     user: { username: 'user' },
   }
+  let comp
 
-  const comp = render(
-    <Blog blog={blog} username="user" onRemove={() => undefined} />,
-  )
+  beforeEach(() => {
+    comp = render(
+      <Blog blog={blog} username="user" onRemove={() => undefined} />,
+    )
+  })
 
-  const infoDiv = comp.container.querySelector('.info')
-  // console.log(prettyDOM(infoDiv))
-  expect(infoDiv).toHaveStyle('display: none')
+  test('renders correctly', () => {
+    const infoDiv = comp.container.querySelector('.info')
+    // console.log(prettyDOM(infoDiv))
+    expect(infoDiv).toHaveStyle('display: none')
+  })
+
+  test('clicking view shows info', () => {
+    const viewButton = comp.getByText('view')
+    fireEvent.click(viewButton)
+    const infoDiv = comp.container.querySelector('.info')
+    expect(infoDiv).not.toHaveStyle('display: none')
+  })
 })
