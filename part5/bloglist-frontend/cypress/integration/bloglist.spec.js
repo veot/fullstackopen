fe671d94c0c7bdb1458a12ed38a13333793ab4d8
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-03-17 15:43:00 +0200

    5.18

--- part5/bloglist-frontend/cypress/integration/bloglist.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'User One',
+      username: 'user1',
+      password: 'user1pass',
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,26 @@ describe('Blog app', function () {
     cy.get('form').contains('username')
     cy.get('form').contains('password')
     cy.get('form').contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('user1')
+      cy.get('#password').type('user1pass')
+      cy.get('form').contains('login').click()
+    })
+
+    it('logged in user can logout', function () {
+      cy.contains('logout').click()
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('user1')
+      cy.get('#password').type('wrong')
+      cy.get('form').contains('login').click()
+      cy.get('.error').should('contain', 'invalid username or password')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.get('.error').should('have.css', 'border-style', 'solid')
+    })
   })
 })
